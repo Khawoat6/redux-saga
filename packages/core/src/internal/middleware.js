d389f56a71aeba2ce7f09c5b4a3881b809aa1d2c
shinima <842351815@qq.com>
2018-07-19 23:13:11 +0800

    fix a small bug

--- packages/core/src/internal/middleware.js
+++ packages/core/src/internal/middleware.js
@@ -15,7 +15,7 @@ export default function sagaMiddlewareFactory(options = {}) {
     }
 
     if (is.notUndef(channel)) {
-      check(channel, is.channel(channel), 'options.channel passed to the Saga middleware is not a channel')
+      check(channel, is.channel, 'options.channel passed to the Saga middleware is not a channel')
     }
   }
 
